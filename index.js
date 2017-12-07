2bcba09d1613b42aff515a7c53f1af45f8b9f854
sir_ingvarr <dev.igorberezin@gmail.com>
2017-12-07 15:10:13 +0200

    repl, 1st task

---
+++ index.js
@@ -0,0 +1,36 @@
+(function() {
+  let fs = require('fs');
+  let repl = require('repl');
+
+  let { log, error, warn, info } = console; 
+  let files;
+
+  fs.readdir('./tasks', (err, fileList) => {
+    files = fileList;
+    if(err) error('Error while trying to read tasks dir ', err);
+    info('Select task to launch: ( .opentask |task_number| )');
+    for(let i = 0; i < fileList.length; i++) {
+      log(`${fileList[i].slice(0, -3)}`);
+    }
+  })
+
+  let replServer = repl.start({ prompt: '--: ' });
+
+  replServer.defineCommand('opentask', {
+    help: 'task number as param',
+    action(number) {
+      try {
+        let taskModule = require(`./tasks/${files[number-1]}`);
+        typeof taskModule === 'function' ? 
+        (() => {
+          console.time('taskTime')
+          taskModule();
+          console.timeEnd('taskTime');
+        })()
+            : log(taskModule); 
+      } catch (err) {
+        error(err);
+      }
+    }
+  })
+})();
